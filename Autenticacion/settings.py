7c0bda497f19b1ef36222b570ed7301cf78d4ef9
Angel Larios <lars50593@gmail.com>
2022-05-27 11:01:31 -0600

    se creo la apliacion del proyecton

--- Autenticacion/settings.py
+++ Autenticacion/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registro',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
