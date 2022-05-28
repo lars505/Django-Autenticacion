8022d419849b36848737a5d958e8a6c44745d55b
Angel Lars <lars50593@gmail.com>
2022-05-28 01:21:22 -0600

    templates de login y register y las rutas

--- registro/urls.py
+++ registro/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
 ]
--- registro/views.py
+++ registro/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'registro/index.html')
+    
+def register(request):
+    return render(request, 'registro/registro.html')
+
+def login(request):
+    return render(request, 'registro/login.html')
